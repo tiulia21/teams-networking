5aadf86f08830e4d6b1849b864b255a76f1168cc
Todea Iulia <iulia21ana@gmail.com>
2024-02-19 09:35:19 -1000

    loadTeams from API

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+import "./style.css";
 console.info("app ready");
+
+function loadTeams() {
+  const promise = fetch("http://localhost:3000/teams-json")
+    .then(r => r.json())
+    .then(teams => {
+      console.table(teams);
+      return teams;
+    });
+  console.warn("loadTeams", promise);
+}
+loadTeams();
